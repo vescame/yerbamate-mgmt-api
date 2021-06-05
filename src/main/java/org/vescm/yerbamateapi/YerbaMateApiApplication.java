90fe9208f6eec514c5e48645a3d2247446b19f04
Vinicius Escame <v.escame@gmail.com>
2021-06-04 22:04:12 -0300

    Initial commit

---
+++ src/main/java/org/vescm/yerbamateapi/YerbaMateApiApplication.java
@@ -0,0 +1,13 @@
+package org.vescm.yerbamateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerbaMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerbaMateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vescm/yerbamateapi/YerbaMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vescm.yerbamateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YerbaMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
