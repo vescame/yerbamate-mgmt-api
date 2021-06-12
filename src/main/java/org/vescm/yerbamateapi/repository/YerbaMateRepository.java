4e0c9b86dab6ce4eaffa380dbebdec301d6246d1
Vinicius Escame <v.escame@gmail.com>
2021-06-12 16:29:08 -0300

    Add repositories

---
+++ src/main/java/org/vescm/yerbamateapi/repository/YerbaMateRepository.java
@@ -0,0 +1,9 @@
+package org.vescm.yerbamateapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.vescm.yerbamateapi.model.YerbaMate;
+
+@Repository
+public interface YerbaMateRepository extends JpaRepository<YerbaMate, Long> {
+}
