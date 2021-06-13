4eb386fdcdf2cddd5a650b270a695e05fbf74b99
Vinicius Escame <v.escame@gmail.com>
2021-06-12 22:55:09 -0300

    Fix missing get/setters

--- src/main/java/org/vescm/yerbamateapi/model/Comment.java
+++ src/main/java/org/vescm/yerbamateapi/model/Comment.java
@@ -19,4 +19,20 @@ public class Comment {
     @NotEmpty
     @Column(nullable = false)
     private String comment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
